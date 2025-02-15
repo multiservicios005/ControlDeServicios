0b6f80a6177f138d13a2f56409912786df2619d9
Luis <multiservicios005@gmail.com>
2025-02-15 12:41:36 -0300

    Versión 20250215 - Se elimina la librería Neodatis y la Clase Test que la usaba.

--- src/main/java/com/servicios5estrellas/service/ReportServices.java
+++ src/main/java/com/servicios5estrellas/service/ReportServices.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import org.neodatis.odb.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
